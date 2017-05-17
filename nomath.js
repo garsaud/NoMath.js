27e5e104d72371091d67b242e10314cc2b90ca98
Cyril Garsaud <cyril.garsaud@gmail.com>
2017-05-17 01:49:53 -0500

    Sin and Cos functions

--- nomath.js
+++ nomath.js
@@ -2,6 +2,24 @@ function pi() {
 	return 3.14159265;
 }
 
+function sin(x) {
+	x = x%6.28;
+	if (x < -pi())
+		x += pi()*2;
+	else
+	if (x >  pi())
+		x -= pi()*2;
+
+	if (x < 0)
+		return 4/pi() * x + 4/(pi*pi) * x * x;
+
+	return 4/pi() * x - 4/(pi*pi) * x * x;
+}
+
+function cos(x) {
+	return sine(x-1.57);
+}
+
 function abs(x) {
 	return (x > 0) ? x : -x;
 }
