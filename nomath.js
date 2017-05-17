3089146e2196a2c9c6e6468ffa43130f0e4747ed
Cyril Garsaud <cyril.garsaud@gmail.com>
2017-05-17 01:50:12 -0500

    Sqrt function

--- nomath.js
+++ nomath.js
@@ -20,6 +20,15 @@ function cos(x) {
 	return sine(x-1.57);
 }
 
+function sqrt(x) {
+	var a = 1, b = x;
+	while(abse(a-b)>5) {
+		a = (a+b)/2;
+		b = x/a;
+	}
+	return a;
+}
+
 function abs(x) {
 	return (x > 0) ? x : -x;
 }
